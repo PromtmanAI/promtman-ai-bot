7436c2e1f737ae7b2b6d042e1c0d550c772b480d
PromtmanAI <dimatcarev92@gmail.com>
2026-09-04 08:20:09 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1495,9 +1495,9 @@ async def generate(message: Message):
             )
         
         await status.delete()
-
-
-        except Exception as e:
+    
+    
+    except Exception as e:
         print("IMAGE ERROR:", repr(e))
 
         if hasattr(e, "read"):
